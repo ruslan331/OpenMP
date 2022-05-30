07560c2de2cc8e95013c0d4da35c779618ff6680
rwandrok@gmail.com <rwandrok@gmail.com>
2022-05-30 10:29:10 +0300

    Добавьте файлы проекта.

---
+++ OpenMP/main.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <omp.h>
+
+#include "timer.hpp"
+#include "solver.hpp"
+#include "test_runner.hpp"
+
+using namespace std;
+
+void SimpleTestSeq() {
+    LCSSolver solver;
+    ASSERT_EQUAL(solver.SolveLessMemory(1, "a", "b"), 0);
+    ASSERT_EQUAL(solver.SolveLessMemory(3, "acb", "abc"), 2);
+    ASSERT_EQUAL(solver.SolveLessMemory(4, "acbe", "acbe"), 4);
+    ASSERT_EQUAL(solver.SolveLessMemory(5, "abcba", "cabab"), 3);
+}
+
+void SimpleTestParallel() {
+    LCSSolver solver;
+    ASSERT_EQUAL(solver.SolveParallelLessMemory(1, "a", "b"), 0);
+    ASSERT_EQUAL(solver.SolveParallelLessMemory(3, "acb", "abc"), 2);
+    ASSERT_EQUAL(solver.SolveParallelLessMemory(4, "acbe", "acbe"), 4);
+    ASSERT_EQUAL(solver.SolveParallelLessMemory(5, "abcba", "cabab"), 3);
+}
+
+void LargeTest() {
+    srand(time(NULL));
+    const int N = 1e4;
+    string s1, s2;
+    s1.reserve(N);
+    s2.reserve(N);
+
+    for (int i = 0; i < N; ++i) {
+        char ch;
+        ch = static_cast<char>(rand() % 26) + 'a';
+        s1.push_back(ch);
+        ch = static_cast<char>(rand() % 26) + 'a';
+        s2.push_back(ch);
+    }
+
+    LCSSolver solver;
+    int res1, res2;
+    {
+        LOG_DURATION("parallel");
+        res2 = solver.SolveParallelLessMemory(N, s1, s2);
+    }
+    
+
+}
+
+void Testing() {
+    TestRunner tr;
+    RUN_TEST(tr, SimpleTestSeq);
+    RUN_TEST(tr, SimpleTestParallel);
+    RUN_TEST(tr, LargeTest);
+}
+
+int main(int argc, char* argv[]) {
+
+    omp_set_dynamic(false);
+    omp_set_num_threads(2);
+
+    Testing();
+
+    return 0;
+}
